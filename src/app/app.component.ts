47a8251b753186d6d17c16bf365831a8bf7db8f2
Inga <inga.s6699@gmail.com>
2022-01-03 13:24:25 +0100

    feat: adds http call from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TodosService } from './shared/services/todos.service';
+import { Todo } from './shared/todo.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'testing-playground';
+
+  todos: Todo[] | any = [];
+
+  constructor(private readonly todoService: TodosService) {
+    this.todoService.getTodos().subscribe((data) => {
+      console.log(data);
+      this.todos = data;
+    });
+  }
 }
