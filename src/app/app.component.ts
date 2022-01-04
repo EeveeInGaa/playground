05c1410435a5c6b7254cd5fa3b9844c4faa7cdbe
Inga <inga.s6699@gmail.com>
2022-01-04 16:23:17 +0100

    feat: adds drag and drop from angular/material (works cool but not as expected)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodosService } from './shared/services/todos.service';
 import { Todo } from './shared/todo.interface';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,18 @@ export class AppComponent {
   todos: Todo[] | any = [];
   searchValue: string = '';
 
+  numbers: number[] = [];
+
   constructor(private readonly todoService: TodosService) {
     this.todoService.getTodos().subscribe((data) => {
-      this.todos = data;
+      this.todos = data; //the data from the api are getting attached to the array to display the data
     });
+    for (let i = 0; i < 11; i++) {
+      this.numbers.push(i);
+    }
+  }
+
+  drop(event: CdkDragDrop<number[]>) {
+    moveItemInArray(this.numbers, event.previousIndex, event.currentIndex);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -6,11 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SearchfilterPipe } from './shared/searchfilter.pipe';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, SearchfilterPipe],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    DragDropModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
